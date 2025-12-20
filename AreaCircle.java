71f36f49b971ae2e1589327b01b655c90ee2b4ac
ShaikhAbida103 <shaikhabi087@gmail.com>
2025-12-20 01:22:02 -0800

    Day1 Java practice

---
+++ AreaCircle.java
@@ -0,0 +1,10 @@
+package Day1_Basics;
+
+public class AreaCircle {
+    public static void main(String[] args) {
+        double r = 5;
+        double area = 3.14 *r*r;
+        System.out.println("Area ="+ area);
+    }
+    
+}
